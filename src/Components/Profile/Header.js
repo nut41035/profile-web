929eb3240a5102b175de33a025d2a4fc910c6ff5
Natsu Kagamine <suphawit.w@kbtg.tech>
2018-07-17 14:27:34 +0700

    update BG

--- src/Components/Profile/Header.js
+++ src/Components/Profile/Header.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 class Header extends Component {
   render() {
-
     if (this.props.data) {
       var name = this.props.data.iam;
       var occupation = this.props.data.occupation;
       var description = this.props.data.description;
       var city = this.props.data.address.city;
-      var networks = this.props.data.social.map(function (network) {
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
+      var networks = this.props.data.social.map(function(network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className} />
+            </a>
+          </li>
+        );
+      });
     }
 
     return (
       <header id="top">
-
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
             Show navigation
@@ -32,7 +36,7 @@ class Header extends Component {
               </NavLink>
             </li>
             <li>
-              <NavLink to="/paper" >
+              <NavLink to="/paper">
                 <span>Paper</span>
               </NavLink>
             </li>
@@ -47,18 +51,19 @@ class Header extends Component {
         <div className="row banner">
           <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3>
+              I'm a {city} based <span>{occupation}</span>. {description}.
+            </h3>
             <hr />
-            <ul className="social">
-              {networks}
-            </ul>
+            <ul className="social">{networks}</ul>
           </div>
         </div>
 
         <p className="scrolldown">
-          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle" />
+          </a>
         </p>
-
       </header>
     );
   }
